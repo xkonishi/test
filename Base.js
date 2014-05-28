ab666e1b4ed05999e88009f30d5c29e7218a7174
xkonishi <konishi.cnb@gmail.com>
2014-05-28 11:50:50 +0900

    Update Base.js

--- Base.js
+++ Base.js
@@ -2,13 +2,9 @@ Ext.define('MyApp.override.form.field.Base', {
     override: 'Ext.form.field.Base',
     
     onFocus: function(e) {
-    	var t = e.currentTarget;
-        console.log('onFocus!! ['+t.id+']');
-        Ext.get(t).focus({delay:100});
-    },
-    onBlur: function(e) {
-    	var t = e.currentTarget;
-        console.log('onBlur!! ['+t.id+']');
-    }
-    
+        console.log('onFocus!!');
+        
+        var me = this;
+        mefocus({delay:100});
+    } 
 });
