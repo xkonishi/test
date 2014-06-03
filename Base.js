15f56308ce80a2d324e61c88a2ac1515a0c5e402
xkonishi <konishi.cnb@gmail.com>
2014-06-03 14:39:15 +0900

    Update Base.js

--- Base.js
+++ Base.js
@@ -1,11 +1,17 @@
 Ext.define('MyApp.override.form.field.Base', {
-    override: 'Ext.form.field.Base',
+     override: 'Ext.form.field.Base',
     
     onFocus: function(e) {
-        console.log('onFocus!!');
-        
-        var me = this;
-        me.callParent(arguments);
+    	var me = this;
+    	me.callParent(arguments);
+
+        console.log('onFocus!!', me);
+
+        //グリッドの編集終了
+        MyApp.view.GridUtil.completeEdit();
+
+        //フォーカス設定
         me.focus({delay:100});
-    } 
+    }
+    
 });
